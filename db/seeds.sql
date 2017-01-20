8a1701582e6f71784a7d42e577bcd33360c30446
LAPTOP-2ALLR6GQ\nhoon <nhoon2002@ucla.edu>
2017-01-20 12:40:18 -0800

    Moved all contents of burger to this one because heroku deployment

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Original Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Protein Burger');
